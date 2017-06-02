73b1d8fbe5699a6264ff9bd966557a185445c7da
Jolin Cai <qooe212156822000@gmail.com>
2017-06-02 23:57:35 +0800

    Print the GUI

--- Java-Homework/CE1002/MIDIPiano-dev/src/edu/american/Controller.java
+++ Java-Homework/CE1002/MIDIPiano-dev/src/edu/american/Controller.java
@@ -4,43 +4,59 @@ import java.io.File;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 public class Controller {
-    @FXML
-    private ImageView WhiteKey;
-    @FXML
-    private ImageView BlackKey;
-    @FXML
+	@FXML
     private Button PlayButton;
     @FXML
     private Button RetryButton;
-    private int i = 1;
+    @FXML
+    private Pane pane;
+    private int Music_Scale = 3;
+    private int Num_Keys = 7;
+    private String[] keys = {"c","d","e","F","G","A","B"};
+    
     @FXML
     private void initialize() {
     	PlayButton.setText("開始彈奏");
     	RetryButton.setText("重新播放");
-    	File file = new File("src/images/a.png");
-        Image image = new Image(file.toURI().toString());
-        WhiteKey.setImage(image);
-        
-        file = new File("src/images/blackKey.png");
-        image = new Image(file.toURI().toString());
-        
-        BlackKey.setImage( image );
-        PlayButton.setOnAction( new EventHandler<ActionEvent> () {
-            @Override
-            public void handle ( ActionEvent event ) {
-            	i = -i;
-            }
-        } );
+    	Stage stage  = new Stage();
+    	int x = 55;
+    	int y = 20;
+    	Group root = new Group();
+    	Scene scene = new Scene(root);
+        HBox box = new HBox();
+    	for(int i = 0;i < Music_Scale ; i++){
+    		for(int j = 0;j< Num_Keys ;j++){
+    			ImageView imageView = new ImageView(new Image("images/"+keys[j]+".png"));
+    			imageView.relocate(x, y);
+    			box.getChildren().add(imageView);
+    		}
+    	}
+    	scene.setFill(Color.BLACK);
+    	root.getChildren().add(box);
+    	
         
     }
     public void setScene(Scene scene){
+    	
+        /*
+    	stage.setTitle("ImageView");
+        stage.setScene(scene); 
+        stage.sizeToScene(); 
+        stage.show(); 
+        */
+    	/*
         scene.setOnKeyPressed ( new EventHandler<KeyEvent> () {
             @Override
             public void handle ( KeyEvent event ) {
@@ -61,5 +77,6 @@ public class Controller {
                 }
             }
         } );
+        */
     }
 }
--- Java-Homework/CE1002/MIDIPiano-dev/src/edu/american/Piano.java
+++ Java-Homework/CE1002/MIDIPiano-dev/src/edu/american/Piano.java
@@ -14,7 +14,6 @@
 
 package edu.american;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -23,7 +22,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -46,9 +47,6 @@ import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 
-import org.jfugue.Pattern;
-import org.jfugue.Player;
-import org.jfugue.Rhythm;
 
 import edu.american.Controller;
 
@@ -66,22 +64,71 @@ import edu.american.Controller;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.jfugue.player.Player;
 
 public class Piano extends Application {
-
+	private int Music_Scale = 3;
+    private int Num_Keys = 7;
+    private String[] keys = {"C","D","E","F","G","A","B"};
+    private String[] octave = {"4","5","6"};
+    private Player player = new Player();
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/piano.fxml"));
         Controller controller = new Controller ();
         fxmlLoader.setController(controller);
         Parent root = fxmlLoader.load();
+        
+        Group group = new Group();
+        HBox box = new HBox(3);
+        box.setPadding(new Insets(50, 12, 15, 12));
+        
+        int x = 0;
+    	int y = 0;
+    	
+    	for(int i = 0;i < Music_Scale ; i++){
+    		for(int j = 0;j< Num_Keys ;j++){
+    			/*
+    			if(j!=0 && j!=3){
+    				ImageView imageView2 = new ImageView(new Image("images/blackKey.png"));
+    				box.getChildren().add(imageView2);
+    				imageView2.setX(x);
+    				imageView2.setY(y);
+    			}
+    			*/
+    			final ImageView imageView = new ImageView(new Image("images/"+keys[j]+".png"));
+    			imageView.setId(keys[j]+octave[i]);
+    	        box.getChildren().addAll(imageView);
+    	        imageView.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+					@Override
+					public void handle(MouseEvent arg0) {
+						// TODO Auto-generated method stub
+						
+						player.play(imageView.getId());
+						System.out.println(imageView.getId());
+					}
+    	        });
+    		}
+    	}
+    	group.getChildren().add(box);
+    	group.getChildren().add(root);
+    	Scene piano = new Scene (group);
+    	piano.setFill(Color.GRAY);
         primaryStage.setTitle("Fx Piano");
-        Scene scene = new Scene ( root, 600, 400 );
-        controller.setScene ( scene );
-        primaryStage.setScene(scene);
+        
+        piano.getStylesheets().add("piano.css");
+        controller.setScene ( piano );
+        primaryStage.setScene( piano);
         primaryStage.show();
     }
 
